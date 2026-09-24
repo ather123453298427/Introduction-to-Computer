a9c0c4d734eeb379c385442241163d7cc530b2b5
ather123453298427 <160517712+ather123453298427@users.noreply.github.com>
2026-09-24 09:28:54 -0700

    Update 1.c

--- 1.c
+++ 1.c
@@ -4,3 +4,15 @@ int main ()
   printf("Introduction to Computers/n");
   printf(" A computer is an electronic device that accepts data as input, processes it, stores it, and produces useful information as output.
 Computers are used in education, business, healthcare, banking, communication, and entertainment.");
+
+
+printf("Types of Computers/n
+
+Supercomputers are Used for complex calculations and scientific research.
+Mainframe Computers are Used by large organizations to process large amounts of data.
+Personal Computers are Used by individuals, such as desktops and laptops.
+Embedded Computers are Built into devices such as cars and washing machines.");
+
+
+  
+  }
