72e6aee0c1082afea93e7a5e8a614bec1ca8f548
ather123453298427 <160517712+ather123453298427@users.noreply.github.com>
2026-09-24 09:24:48 -0700

    Create 1.c

---
+++ 1.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+int main ()
+{
+  printf("Introduction to Computers/n");
+  printf(" A computer is an electronic device that accepts data as input, processes it, stores it, and produces useful information as output.
+Computers are used in education, business, healthcare, banking, communication, and entertainment.");
